7e2f893ce967373b19cc627329a7fd166f6b8756
krakeljur <sasbot.zver@mail.ru>
2024-06-05 14:42:36 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+
+buildscript {
+
+    repositories {
+        google()
+    }
+
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+}
